cc007efd5e5de0b00e367901c69c6d91a8f03d27
Nivya-235693 <112929554+Nivya-235693@users.noreply.github.com>
2022-10-07 12:35:57 +0530

    Initial commit

---
+++ ex3.js
@@ -0,0 +1,4 @@
+var numbers = [1, 2, 3]; // inferred to type number[]
+numbers.push(4); // no error
+// comment line below out to see the successful assignment
+var head = numbers[0];
---
+++ ex3.ts
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3]; // inferred to type number[]
+numbers.push(4); // no error
+// comment line below out to see the successful assignment
+let head: number = numbers[0];
